a02758c21050609a1cd5920dcee7e7fcc214a008
Enrico Cerroni <e.cerroni@gmail.com>
2020-07-06 11:33:53 +0200

    Implement user validation from fauna db

--- src/dataconnectors/index.js
+++ src/dataconnectors/index.js
@@ -1,7 +1,9 @@
 const faunadb = require('faunadb')
-console.log('SSSSSSSSSSSSSS', process.env.FAUNA_SECRET);
 const q = faunadb.query
-const client = new faunadb.Client({ secret: process.env.FAUNA_SECRET || FAUNA_SECRET })
+const client = new faunadb.Client({
+  secret: process.env.FAUNA_SECRET || FAUNA_SECRET,
+  fetch: fetch.bind(globalThis)
+})
 
 
 const dbClient = {
--- src/datasources/db/User/_utils.js
+++ src/datasources/db/User/_utils.js
@@ -1,15 +1,51 @@
-const users = require('../../../mocks/_users');
-const { ERROR } = require('../../../environment/_errors');
+const to = require('await-to-js')
+// const Random = require('random-seed-generator'); // TODO: update the token every time a user update his password
+const { fql, q } = require('../../../dataconnectors')
+// const users = require('../../../mocks/_users')
+const { ERROR } = require('../../../environment/_errors')
+const identifyUser = async (username, password) => {
+  const [err, user] = await to.default(fql.query( // chatch error con await to()
+    q.Login(
+      q.Match(q.Index("users_by_email"), username),
+      { password },
+    )
+  ))
+  if (!err) {
+    const { instance: { value: { id: userId } = {} } = {}} = user
+    const validUser = await fql.query(q.Get(q.Ref(q.Collection('users'), userId)))
+    
+    if (validUser) {
+      const { data: { email, role, token, delta } = {} } = validUser
+      return {
+        id: userId,
+        password: token,
+        email,
+        role,
+        delta,
+      }
+    }
+  }  
+  return undefined
+}
+
+const retrieveUser = async (id) => {
+  const validUser = await fql.query(q.Get(q.Ref(q.Collection('users'), id)))
+  const { data: { delta, token: password } = {} } = validUser
+  return {
+    delta,
+    password
+  }
+}
 
 module.exports = {
   validate: async (username, password) => {
-    const validUser =
-      users.filter(u => u.username === username).length > 0
-        ? users.filter(u => u.username === username)[0]
-        : undefined;
+    // const validUser =
+      // users.filter(u => u.username === username).length > 0
+      //   ? users.filter(u => u.username === username)[0]
+      //   : undefined;
+    let validUser = await identifyUser(username, password)
     if (validUser) {
-      const validPassword = password === validUser.password
-      
+      const validPassword = validUser.password
       if (!validPassword) {
         throw new Error(ERROR.USER.WRONG_PASSWORD);
       }
@@ -18,10 +54,7 @@ module.exports = {
     throw new Error(ERROR.USER.WRONG_CREDENTIALS);
   },
   getPassword: async ({ id, delta = false }) => {
-    const validUser =
-      users.filter(u => (u.id === id) || (u._id === id)).length > 0
-        ? users.filter(u => (u.id === id) || (u._id === id))[0]
-        : undefined;
+    const validUser = await retrieveUser(id)
     if (validUser) {
       const response = {
         password: validUser.password,
